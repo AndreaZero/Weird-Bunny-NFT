43d5ad47288cab68b2aca8379e9770e8f83ffff0
0xZeroBaby <100024338+0xZeroBaby@users.noreply.github.com>
2022-03-13 11:48:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
+import { ethers } from "ethers";
 
 const truncate = (input, len) =>
   input.length > len ? `${input.substring(0, len)}...` : input;
